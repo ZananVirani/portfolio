b1ce7014a6e5e899cfb4de6ffe840307f2d02220
Zanan Virani <viranizanan@gmail.com>
2025-07-14 14:12:04 -0400

    Changed landing titles

--- src/components/animations/Type.tsx
+++ src/components/animations/Type.tsx
@@ -9,7 +9,7 @@ function Type() {
       options={{
         strings: [
           "Software Engineer",
-          "Explorer",
+          "Curious Learner",
           "Full-Stack Dev",
           "Driven by Passion",
         ],
